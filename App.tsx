398721b3296de6c44a6dd4b43154d90ed3dc5bbd
ahauter <hauterame.com>
2024-05-17 15:03:27 -0700

    Data type definition

--- App.tsx
+++ App.tsx
@@ -1,11 +1,37 @@
+import React from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Clock } from './clock';
+import { TimeBlock } from './dataTypes';
+const timeBlocks: TimeBlock[] = [
+  {
+    duration: 15 * 1000 * 60,
+    startTime: new Date("2024-05-18T14:30Z"),
+    tasks: [],
+    category: "work",
+    subTimeBlocks: []
+  },
+  {
+    duration: 15 * 1000 * 60,
+    startTime: new Date("2024-05-18T14:45Z"),
+    tasks: [],
+    category: "work",
+    subTimeBlocks: []
+  },
+  {
+    duration: 15 * 1000 * 60,
+    startTime: new Date("2024-05-18T15:00Z"),
+    tasks: [],
+    category: "work",
+    subTimeBlocks: []
+  }
+]
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <Text>Hello World</Text>
+      <Clock timeBlocks={timeBlocks} />
       <StatusBar style="auto" />
     </View>
   );
---
+++ clock.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Text } from 'react-native'
+import { TimeBlock } from "./dataTypes";
+interface ClockProps {
+    timeBlocks: TimeBlock[]
+}
+export function Clock({ timeBlocks }: ClockProps) {
+    return <Text>Hello</Text>
+}
