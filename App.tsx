a2c988a82ab8574394b99922dc5b842d45ec75a8
Austin <hautera@me.com>
2024-05-19 07:44:05 -0700

    Generate timeblocks

--- App.tsx
+++ App.tsx
@@ -2,35 +2,14 @@ import React from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Clock } from './clock';
-import { TimeBlock } from './dataTypes';
-const timeBlocks: TimeBlock[] = [
-  {
-    duration: 15 * 1000 * 60,
-    startTime: new Date("2024-05-18T14:30Z"),
-    tasks: [],
-    category: "work",
-    subTimeBlocks: []
-  },
-  {
-    duration: 15 * 1000 * 60,
-    startTime: new Date("2024-05-18T14:45Z"),
-    tasks: [],
-    category: "work",
-    subTimeBlocks: []
-  },
-  {
-    duration: 15 * 1000 * 60,
-    startTime: new Date("2024-05-18T15:00Z"),
-    tasks: [],
-    category: "work",
-    subTimeBlocks: []
-  }
-]
+import { generateTimeBlocksForDay } from './utils';
+
 export default function App() {
+  const currentTimeBlocks = generateTimeBlocksForDay();
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <Clock timeBlocks={timeBlocks} duration={1000 * 60 * 60 * 24} />
+      <Clock timeBlocks={currentTimeBlocks} duration={1000 * 60 * 60 * 24} />
       <StatusBar style="auto" />
     </View>
   );
---
+++ utils/index.ts
@@ -0,0 +1,47 @@
+import { TimeBlock } from "../dataTypes";
+import { useState } from 'react'
+function getTimeBlockCategory(hour: number): string {
+  if (hour < 7 || hour > 21) return "sleep";
+  if (hour > 9 && hour < 17) return "work";
+  return "idk man";
+}
+
+/**
+  * Returns a list of hour-long timeblocks for the current date
+  */
+export function generateTimeBlocksForDay(): TimeBlock[] {
+  //just doing hours for now 
+  const result: TimeBlock[] = [];
+  const hourLength = 1000 * 60 * 60;
+  for (let i = 0; i < 24; i++) {
+    const startDate = new Date();
+    startDate.setHours(i, 0, 0, 0);
+    const t: TimeBlock = {
+      startTime: startDate,
+      duration: hourLength,
+      category: getTimeBlockCategory(i),
+      tasks: [],
+      subTimeBlocks: []
+    }
+    result.push(t);
+  }
+  return result;
+}
+
+export function useClockInfo() {
+  const [timeStr, setTimeStr] = useState("");
+  const [angle, setAngle] = useState(0);
+  const dayLength = 1000 * 60 * 60 * 24;
+  const getTimeStr = () => {
+    const timeString = (new Date()).toLocaleTimeString();
+    setTimeStr(timeString);
+    const dayStart = new Date()
+    dayStart.setHours(0, 0, 0, 0);
+    const currentH = new Date();
+    const diff = currentH.getTime() - dayStart.getTime();
+    const ang = diff / dayLength * 360;
+    setAngle(ang);
+  };
+  setInterval(getTimeStr, 100);
+  return { timeStr, handAngle: angle };
+}
