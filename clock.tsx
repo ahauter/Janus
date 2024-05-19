1c865b3f30d0692ec4f15506acc8943c9582f22e
Austin <hautera@me.com>
2024-05-19 07:44:49 -0700

    Clock looks like a clock

--- clock.tsx
+++ clock.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
-import { Svg, Circle, Polygon, Rect } from 'react-native-svg';
+import {  View, StyleSheet } from 'react-native'
+import { Svg, Circle, Text, Polygon, ClipPath, Defs, G, Line } from 'react-native-svg';
 import { TimeBlock } from "./dataTypes";
+import { useClockInfo } from './utils';
 
 
 interface ClockProps {
@@ -10,26 +11,110 @@ interface ClockProps {
     duration: number
 }
 
+function blockColor(block: TimeBlock) {
+  switch (block.category) {
+    case "sleep":
+      return "blue"
+    case "work":
+      return "orange"
+    default:
+      return "gray"
+  }  
+}
+
 export function Clock({ timeBlocks, duration }: ClockProps) {
-  const colors = ["red", "blue", "yellow", "purple", "orange"]
-  const size = 120
+  const {timeStr, handAngle} = useClockInfo();
+  const size = 420
   const radius = size / 2
-  const rotation = 360 / colors.length
-  const vertexOffset = Math.round(radius * Math.tan(Math.PI / colors.length))
-  const timeRegions = colors.map((color: string, index: number) => {
+  const rotation = 360 / timeBlocks.length
+  const vertexOffset = Math.round(radius * Math.tan(Math.PI / timeBlocks.length))
+  const timeRegions = timeBlocks.map((block: TimeBlock, index: number) => {
     const curRotation = rotation * index
     return <Polygon
+      key={`timeBlock-${index}`}
       points={`${radius + vertexOffset} 0, ${radius - vertexOffset} 0, ${size /2} ${size /2}`}
       rotation={curRotation}
       originX={radius}
       originY={radius}
-      fill={color}>
-      
+      fill={blockColor(block)}>
+      <Text
+        fill="black" 
+        stroke="black"
+        textAnchor="middle"
+        fontSize={5}
+        x={0}
+        y={radius/4}
+      >{index}</Text>
     </Polygon>       
   });
+  
+  const regionDividers = [];
+  for (let i = 0; i < timeBlocks.length; i ++) {
+    const curRotation = rotation * i + rotation / 2
+    regionDividers.push(<Line 
+      key={`divider-${i}`}
+      x1={size/2}                   
+      y1={size/2}                   
+      x2={size/2}                   
+      y2={0}                   
+      originX={radius}
+      originY={radius}
+      stroke={"black"}
+      strokeWidth={1}
+      rotation={curRotation}
+    />)
+  }
   return <View>
-    <Svg width={size + 10} height={size + 10} >
+    <Svg width={size + 10} height={size + 10}>
+    <Defs>    
+      <ClipPath id="outerClip">
+        <Circle
+          cx={size/2}
+          cy={size/2}
+          r={0.95 * radius}
+        />
+      </ClipPath>
+    </Defs>    
+    <G clipPath='url(#outerClip)'>
       {timeRegions}
-    </Svg>
+    </G>
+    <Circle
+      fill="white"
+      cx={size/2}
+      cy={size/2}
+      r={0.65 * radius}
+    />
+    <G clipPath='url(#outerClip)'>
+      {regionDividers} 
+      <Line 
+        x1={size/2}                   
+        y1={size/2}                   
+        x2={size/2}                   
+        y2={0}                   
+        originX={radius}
+        originY={radius}
+        stroke={"purple"}
+        strokeWidth={5}
+        rotation={Math.round(handAngle)}
+      />
+    </G>
+    <Circle
+      fill="white"
+      stroke="black"
+      strokeWidth={2}
+      cx={size/2}
+      cy={size/2}
+      r={0.25 * radius}
+    />
+    <Text
+      fill="black"
+      stroke="black"
+      fontSize="20"
+      x={radius}
+      y={radius + 10}
+      textAnchor="middle" >
+     {timeStr}
+    </Text>
+  </Svg>
   </View>
 }
