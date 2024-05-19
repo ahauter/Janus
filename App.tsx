584e308a303c0cee5e4a3bed7a267849eda350dd
JB2120 <jdbraithwaite@hotmail.com>
2024-05-19 17:58:38 +0100

    First draft od Dosha selection

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBarStyle, StyleSheet, Text, View } from 'react-native';
 import { Clock } from './clock';
 import { TimeBlock } from './dataTypes';
 const timeBlocks: TimeBlock[] = [
@@ -29,10 +29,20 @@ const timeBlocks: TimeBlock[] = [
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Hello World</Text>
-      <Clock timeBlocks={timeBlocks} />
-      <StatusBar style="auto" />
+      <StatusBar animated />
+      <View style={styles.titleBox}>
+        <Text style={styles.title}>Choose Your Dosha</Text>
+        <Text style={styles.subtitle}>{'This will affect when we schedule your most important tasks.\nDon\'t worry you can always change this later'}</Text>
+      </View>
+      <View style={styles.Dosha1}>
+        <Text>Vata</Text>
+      </View>
+      <View style={styles.Dosha2}>
+        <Text>Pitta</Text>
+      </View>
+      <View style={styles.Dosha3}>
+        <Text>Kapha</Text>
+      </View>
     </View>
   );
 }
@@ -43,5 +53,59 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  navBar:{
+    backgroundColor:'#98D369',
+  },
+  titleBox:{
+    backgroundColor:'#98D369',
+    paddingTop:30,
+    marginHorizontal:10,
+    paddingHorizontal:10,
+    width: '100%',
+    height:150,
+    justifyContent: 'center',
+    alignItems:'center',
+
+  },
+  title:{
+    fontSize: 40,
+    paddingBottom:5,
+    fontWeight: 'bold',
+    alignContent: 'center'
+
+  },
+  subtitle:{
+    fontSize:15,
+    color: '#FFF',
+    alignContent: 'center',
+    justifyContent: 'center',
+    textAlign: 'center'
+  },
+  Dosha1:{
+    //flex: 2,
+    backgroundColor:'#ddd',
+    alignItems: 'center',
+    justifyContent:'center',
+    borderRadius:10,
+    margin:30,
+    height: '20%',
+  },
+  Dosha2:{
+    //flex: 3,
+    backgroundColor:'#ddd',
+    alignItems: 'center',
+    justifyContent:'center',
+    borderRadius:10,
+    margin:30,
+    height: '20%',
+  },Dosha3:{
+    //flex: 4,
+    backgroundColor:'#ddd',
+    alignItems: 'center',
+    justifyContent:'center',
+    borderRadius:10,
+    margin:30,
+    height: '20%',
   },
 });
