d5d1b542bece586785675f6f8f218fc7a4bd0825
Lauren <justin208350@berkeley.edu>
2024-05-20 09:02:27 -0400

    routing updates

--- App.tsx
+++ App.tsx
@@ -5,20 +5,59 @@ import { Clock } from './clock';
 import { generateTimeBlocksForDay } from './utils';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Dimensions } from 'react-native';
 
 const Stack = createStackNavigator();
 
 function HomeScreen({ navigation }) {
+  const screen_width = Dimensions.get('window').width * 0.8;
   const currentTimeBlocks = generateTimeBlocksForDay();
   return (
     <View style={styles.container}>
+      <View style={{flexDirection: 'row', marginBottom: 100, width: screen_width, justifyContent: 'space-between' }}>
+        <TouchableOpacity 
+          style={styles.taskButton} 
+          onPress={() => navigation.navigate('Tasks')}
+        >
+          <Text style={styles.buttonText}>Placeholder</Text>
+        </TouchableOpacity>
+        <TouchableOpacity 
+          style={styles.taskButton} 
+          onPress={() => navigation.navigate('Tasks')}
+        >
+          <Text style={styles.buttonText}>Add a task</Text>
+        </TouchableOpacity>
+      </View>
       <Clock timeBlocks={currentTimeBlocks} duration={1000 * 60 * 60 * 24} />
-      <TouchableOpacity 
-        style={styles.button} 
-        onPress={() => navigation.navigate('Tasks')}
-      >
-        <Text style={styles.buttonText}>Tasks</Text>
-      </TouchableOpacity>
+      <View style={{flexDirection: 'row', justifyContent: 'center', width: screen_width, marginTop: 60, backgroundColor: '#007BFF', borderRadius: 10, padding: 5 }}>
+        <TouchableOpacity 
+          style={styles.buttonBottom} 
+          onPress={() => navigation.navigate('Tasks')}
+        >
+          <Text style={styles.smallText}>Current task</Text>
+        </TouchableOpacity>
+        <Text>  |  </Text>
+        <TouchableOpacity 
+          style={styles.buttonBottom} 
+          onPress={() => navigation.navigate('Tasks')}
+        >
+          <Text style={styles.smallText}>Timer</Text>
+        </TouchableOpacity>
+        <Text>  |  </Text>
+        <TouchableOpacity 
+          style={styles.buttonBottom} 
+          onPress={() => navigation.navigate('Tasks')}
+        >
+          <Text style={styles.smallText}>Pause task</Text>
+        </TouchableOpacity>
+        <Text>  |  </Text>
+        <TouchableOpacity 
+          style={styles.buttonBottom} 
+          onPress={() => navigation.navigate('Tasks')}
+        >
+          <Text style={styles.smallText}>Finish task</Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -51,19 +90,25 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginLeft: 8, // not sure why the clock isn't centered, so I have added this to compensate
   },
-  button: {
+  taskButton: {
     backgroundColor: '#007BFF',
     padding: 7,
     paddingLeft: 12,
     paddingRight: 12,
     borderRadius: 10,
-    alignItems: 'center',
+  },
+  buttonBottom: {
+    backgroundColor: '#007BFF',
+    borderRadius: 10,
     justifyContent: 'center',
-    marginTop: 20,
-    marginRight: 13, // added to compensate for off-center clock so that "Tasks" button is centered
+    alignItems: 'center',
   },
   buttonText: {
     color: '#FFFFFF',
     fontSize: 16,
+  },
+  smallText: {
+    color: '#FFFFFF',
+    fontSize: 12,
   },
 });
