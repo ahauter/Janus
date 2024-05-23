d0ee4a9f8e04d0e8f55f8c453c0c5b7972587e7b
Lauren <justin208350@berkeley.edu>
2024-05-22 22:53:07 -0400

    next and accept task

--- taskSelect.tsx
+++ taskSelect.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useMemo, useState } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TouchableOpacity, StyleSheet } from 'react-native';
 import { Task } from './dataTypes';
 import { prioritizeTasks } from './utils';
 import { AppStateContext, AppState, AppDispatch, DispatchContext } from './utils/dataStore';
@@ -20,17 +20,56 @@ export function TaskSelection() {
     const newInd = (taskInd + 1) % incompleteTasks.length;
     setTaskInd(newInd);
   };
-  if (task === undefined) return <View>
+  if (task === undefined) return
+  <View>
     <Text>
       You don't have anything to do!
       Try adding something
     </Text>
   </View>;
   const durationStr = `${task.estimatedDuration.getHours()}h${task.estimatedDuration.getMinutes()}m`;
-  return <View>
-    <Text>{task.name}</Text>
-    <Text>{durationStr}</Text>
-    <Button title='Next Task' onPress={reject} />
-    <Button title='Accept Task' onPress={accept} />
-  </View>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.content}>{task.name}</Text>
+      <Text style={styles.time}>{durationStr}</Text>
+      <Text> </Text>
+      <TouchableOpacity style={styles.button} onPress={reject}>
+        <Text style={styles.buttonText}>Next task</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={accept}>
+        <Text style={styles.buttonText}>Accept task</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    alignItems: 'center',
+  },
+  button: {
+    marginVertical: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    width: 110,
+    backgroundColor: '#007BFF',
+    borderRadius: 10, 
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  content: {
+    fontSize: 20,
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+  time: {
+    fontSize: 16,
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+});
+
